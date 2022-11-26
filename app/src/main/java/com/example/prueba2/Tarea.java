50fee06147841b3b94ed3255033c0fc3f65b0a33
zhang <109382786+123apptest@users.noreply.github.com>
2022-11-26 12:49:03 -0300

    revisar codigo

--- app/src/main/java/com/example/prueba2/Tarea.java
+++ app/src/main/java/com/example/prueba2/Tarea.java
@@ -2,27 +2,33 @@ package com.example.prueba2;
 
 import java.io.Serializable;
 
+// Genera Class Tarea,       // Serializable para facil compatido datos
 public class Tarea implements Serializable {
+    // valiable
     private String titulo;
     private String descripcion;
 
-
+    // modificar Forma de String, ejemplo: Item1  Sub item1
     public String toString() {
         return titulo + "\n" + descripcion;
     }
+
+    //Obtener titulo
     public String getTitulo() {
         return titulo;
     }
 
-
+    // Modificar Titulo
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    // Obtener Descripcion
     public String getDescripcion() {
         return descripcion;
     }
 
+    // Modificar Descripcion
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
@@ -30,3 +36,4 @@ public class Tarea implements Serializable {
 
 
 }
+
